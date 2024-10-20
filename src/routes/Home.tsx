7a6a2bafd47cf16c39fdc75ec309454747873f8a
fralnz <francesco@fragiolanza.eu>
2024-10-20 17:03:41 +0200

    add typed effect

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import pfpImg from "../assets/imgs/pfp.png";
 import gbHand from "../assets/imgs/gbHand.png";
 import "../styles/Home.css";
+import "../styles/Typing.css";
 import ProjectCards from "./components/ProjectCards.tsx";
 
 function Home() {
@@ -8,11 +9,11 @@ function Home() {
         <>
             <section className="speechTitle">
                 <div className="speechBubbleBody">
-                    <h1>Hi, I'm Francesco. I like computers.</h1>
+                    <h1 className="typing-title">Hi, I'm Francesco. I like computers.</h1>
                     <div className="speechBubbleTailBottom"></div>
                 </div>
                 <div>
-                    <img src={pfpImg} className="pfp"/>
+                    <img src={pfpImg} className="pfp" alt="My beautiful face"/>
                 </div>
             </section>
             <section className="introduction">
@@ -25,6 +26,7 @@ function Home() {
                 </div>
                 <img src={gbHand} alt="GameBoy Hand"/>
             </section>
+            <h2>While you're here, check out some of my projects!</h2>
             <ProjectCards/>
         </>
     );
